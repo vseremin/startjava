f66770b96e2499e4e5794f468e5eeac2b7db5663
vseremin <eremvyach@gmail.com>
2022-10-30 21:13:12 +0300

    Небольшие изменения в именах переменных Небольшие изменения в именах переменных

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,19 +1,19 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         System.out.println("Игра \"Угадай число\"");
-        int minValue = 1;
-        int maxValue = 100;
-        int numComp = 17;
-        int numUser = 3;
-        while (numComp != numUser) {
-            if (numUser > numComp) {
-                System.out.println("число " + numUser + " больше того, что загадал компьютер");
-                maxValue = numUser;
-                numUser = (minValue + maxValue) / 2;
-            } else if (numUser < numComp) {
-                System.out.println("число " + numUser + " меньше того, что загадал компьютер");
-                minValue = numUser;
-                numUser = (minValue + maxValue + 1) / 2;
+        int startRange = 1;
+        int endRange = 100;
+        int compNum = 17;
+        int playerNum = 3;
+        while (compNum != playerNum) {
+            if (playerNum > compNum) {
+                System.out.println("число " + playerNum + " больше того, что загадал компьютер");
+                endRange = playerNum;
+                playerNum = (startRange + endRange) / 2;
+            } else if (playerNum < compNum) {
+                System.out.println("число " + playerNum + " меньше того, что загадал компьютер");
+                startRange = playerNum;
+                playerNum = (startRange + endRange + 1) / 2;
             }
         }
         System.out.println("Вы победили!");
