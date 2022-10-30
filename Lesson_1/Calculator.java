e188c7fc2d495293d69da6f86defad8fd263e640
vseremin <eremvyach@gmail.com>
2022-10-30 19:31:11 +0300

    Новые файлы Новые файлы

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Калькулятор");
+        int num1 = 5;
+        int num2 = 7;
+        int result = 0;
+        char sign = '^';
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '%') {
+            result = num1 % num2;
+        } else if (sign == '^') {
+            result = 1;
+            for (int i = 0; i < num2; i++) {
+                result *= num1;
+            }
+        }
+        System.out.println(num1 + " " + sign + " " + num2 + " = "  + result);
+    }
+} 
