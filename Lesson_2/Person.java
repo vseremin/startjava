ba9c5831f5e78802447b2366fa9e25fdfe1251ce
vseremin <eremvyach@gmail.com>
2022-10-31 18:19:48 +0300

    новые файлы новые файлы

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "Vyacheslav";
+    String sex = "male";
+    double height = 1.89;
+    double weight = 87.5;
+    int age = 31;
+
+    void go() {
+        System.out.println("Идет");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void speak() {
+        System.out.println("Говорит");
+    }
+
+    void learnJava() {
+        System.out.println("Изучает java");
+    }
+} 
