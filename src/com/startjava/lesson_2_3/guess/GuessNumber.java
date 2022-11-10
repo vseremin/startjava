0d72adbb3a7af78a14bbc414b5f18b07cba175a9
vseremin <eremvyach@gmail.com>
2022-11-10 20:16:06 +0300

    изменение структуры файлов и папок изменение структуры файлов и папок

--- src/com/startjava/lesson_2_3/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3/guess/GuessNumber.java
@@ -1,3 +1,4 @@
+package com.startjava.lesson_2_3.guess;
 import java.util.Scanner;
 import java.util.Random;
 
--- src/com/startjava/lesson_2_3/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3/guess/GuessNumberTest.java
@@ -1,3 +1,4 @@
+package com.startjava.lesson_2_3.guess;
 import java.util.Scanner;
 
 public class GuessNumberTest {
