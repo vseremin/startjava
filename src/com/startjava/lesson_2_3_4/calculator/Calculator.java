29c5efffb6cf19cdd53132b23619e02de63ae9f4
vseremin <eremvyach@gmail.com>
2022-11-15 19:42:58 +0300

    небольшие изменения небольшие изменения

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -5,11 +5,9 @@ public class Calculator {
     private int num1;
     private int num2;
     private char sign;
-    private String expression;
 
     public int calculate(String expression) {
-        this.expression = expression;
-        parseExpression();
+        parseExpression(expression);
         switch (sign) {
             case '+': 
                 return num1 + num2; 
@@ -29,10 +27,10 @@ public class Calculator {
         }
     }
 
-    private void parseExpression() {
-        String[] lines = expression.split(" ");
-        num1 = Integer.parseInt(lines[0]);
-        sign = lines[1].charAt(0);
-        num2 = Integer.parseInt(lines[2]);
+    private void parseExpression(String expression) {
+        String[] partsExpression = expression.split(" ");
+        num1 = Integer.parseInt(partsExpression[0]);
+        sign = partsExpression[1].charAt(0);
+        num2 = Integer.parseInt(partsExpression[2]);
     }
 }
